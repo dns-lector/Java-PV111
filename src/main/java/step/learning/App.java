ab665d8048630ede024d1b3da2a9fc5690780c36
DenNikSam <denniksam@gmail.com>
2024-01-24 16:54:59 +0200

    unused method

--- src/main/java/step/learning/App.java
+++ src/main/java/step/learning/App.java
@@ -1,13 +1,29 @@
 package step.learning;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // System.out.println( "Hello World!" );
+        new Basics().run();
     }
 }
+/*
+Java:
+ - JRE (JVM) - середовище виконання
+ - JDK - засоби розробника (~компілятор)
+ - IDE - Intellij Idea (Apache NetBeans, Eclipse, ...)
+
+= Мова транслюючого типу (видає проміжний код - файли .class)
+= GL (покоління) 4, парадигма - ООП, типізація - статична
+= Висока зворотня сумісність
+= Прив'язка до файлової системи:
+ - файли класів повинні називатись так само, як і самі класи (+.java)
+  -> один файл - один клас (public)
+ - пакети (package) - називають як папки, точка у назвах - підпапки
+= Casing
+ - Types: CapitalCamelCase
+ - methods, vars: lowerCamelCase
+ - const: PASCAL_CASE
+ - packages: snake_case
+ */
--- src/main/java/step/learning/Basics.java
+++ src/main/java/step/learning/Basics.java
@@ -1,2 +1,21 @@
-package step.learning;public class Basics {
+package step.learning;
+
+public class Basics {
+    public void run() {
+        System.out.println( "Java basics" );
+        // Змінні та типи даних
+        // є "прімітиви", які поводяться як значимі типи
+        // всі інші типи даних - reference
+        int x = 10 ;
+        Integer xx = 10 ;  // reference-аналог int
+    }
+    private void unused() {
+
+    }
 }
+/*
+Д.З. Встановити Java, засоби розробника, IDE
+Створити проєкт за Maven архетипом quickstart
+Налаштувати Git, опублікувати репозиторій
+Прикласти посилання на репозиторій проєкту.
+ */
