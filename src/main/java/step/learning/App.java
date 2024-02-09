d1772502a1b20c491811d20c60d00737c270a3a8
dns-lector <dns.lector@ukr.net>
2024-02-09 19:36:03 +0200

    DB

--- src/main/java/step/learning/App.java
+++ src/main/java/step/learning/App.java
@@ -1,5 +1,6 @@
 package step.learning;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import step.learning.async.AsyncDemo;
@@ -18,9 +19,14 @@ public class App
         // new OopDemo().run();
         // new AsyncDemo().run();
         // new DbDemo().run();
-        Injector injector = Guice.createInjector( new ServiceModule() ) ;   // модуль IoC
-        IocDemo instance = injector.getInstance( IocDemo.class ) ;  // заміна new - Object Resolve
-        instance.run();
+
+        // Injector injector = Guice.createInjector( new ServiceModule() ) ;   // модуль IoC
+        // IocDemo instance = injector.getInstance( IocDemo.class ) ;  // заміна new - Object Resolve
+        // instance.run();
+        try( ServiceModule services = new ServiceModule() ) {
+            Guice.createInjector( services ).getInstance( DbDemo.class ).run();
+        } catch (Exception ignored) {}
+
     }
 }
 /*
--- src/main/java/step/learning/ioc/ServiceModule.java
+++ src/main/java/step/learning/ioc/ServiceModule.java
@@ -1,6 +1,7 @@
 package step.learning.ioc;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import com.google.inject.name.Names;
 import step.learning.services.hash.HashService;
 import step.learning.services.hash.Md5HashService;
@@ -8,12 +9,16 @@ import step.learning.services.hash.Sha1HashService;
 import step.learning.services.rnd.CodeGenerator;
 import step.learning.services.rnd.DigitCodeGenerator;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Random;
 
 /**
  * Binding interfaces and classes for services
  */
-public class ServiceModule extends AbstractModule {
+public class ServiceModule extends AbstractModule implements AutoCloseable {
     @Override
     protected void configure() {
         // bind(HashService.class).to(Sha1HashService.class);
@@ -29,5 +34,34 @@ public class ServiceModule extends AbstractModule {
         bind( Random.class ).toInstance( new Random() );
 
         bind( CodeGenerator.class ).to( DigitCodeGenerator.class ) ;
+    }
+
+    private Connection connection ;
+    private  Driver mySqlDriver ;
+    @Provides  // методи-провайдери: для кожної точки @Inject Connection .... буде виклик методу
+    private Connection getConnection() {
+        if( connection == null ) {
+            try {
+                mySqlDriver = new com.mysql.cj.jdbc.Driver() ;
+                DriverManager.registerDriver( mySqlDriver ) ;
+                String connectionString = "jdbc:mysql://localhost:3306/java_111" +
+                        "?useUnicode=true&characterEncoding=UTF-8" ;
+                connection = DriverManager.getConnection( connectionString, "user_111", "pass_111" ) ;
+            }
+            catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return connection ;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if( connection != null ) {
+            connection.close() ;
+        }
+        if( mySqlDriver != null ) {
+            DriverManager.deregisterDriver( mySqlDriver ) ;
+        }
     }
 }
